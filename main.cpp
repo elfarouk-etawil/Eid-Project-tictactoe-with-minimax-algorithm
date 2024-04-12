8782a6b58078b4f895a5cbc5cc13bedff80123f8
elfarouk-etawil <122612103+elfarouk-etawil@users.noreply.github.com>
2024-04-12 13:47:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-typedef char Board[3][3];
+typedef char Board[3][3]; // Define a 3x3 board using a two-dimensional array of characters
 
 struct position
 {
-    short i;
-    short j;
+    short i; // Row index
+    short j; // Column index
 };
 
 #include "board.h"
@@ -15,53 +15,54 @@ struct position
 
 int main()
 {
-    system("color 3");
     // Welcome the user
+    system("color 3"); // Set console text to aqua
     cout << "\t-<Welcome to tic tac toe>-" << endl;
     cout << "\tTry to defeat the computer\n" << endl;
     cout << "Press Enter to continue...";
-    cin.get();
-    system("cls");
+    cin.get(); // Wait for user to press Enter
+    system("cls"); // Clear the console screen
 
-    // Print instructions
+    // Print game instructions
     cout << "Instructions: \n"
-    << "1. You starts first!\n"
-    << "2. Use number from 1 to 9 to select your next move!\n"
+    << "1. You start first!\n"
+    << "2. Use numbers from 1 to 9 to select your next move!\n"
     << "Good luck!\n\n";
     cout << "Press Enter to continue...";
-    cin.get();
-    system("cls");
+    cin.get(); // Wait for user to press Enter
+    system("cls"); // Clear the console screen
 
     Board board;
     Computer comp;
-    reset_board(board);
 
-    while(get_winner(board) == ' ' && space_count(board) > 0)
+    game_start: // Label used for restarting the game
+    reset_board(board); // Reset the game board to its initial state
+    while(get_winner(board) == ' ' && space_count(board) > 0) // Continue game until there is a winner or the board is full
     {
-        system("color 6");
-        display_board(board);
+        system("color 6"); // Set console text to yellow
+        display_board(board); // Display the current state of the game board
         short next_move;
-        while(true)
+        while(true) // Loop until a valid move is entered
         {
             cout << "Enter Your Move(1-9): ";
             cin >> next_move;
-            cin.clear();
+            cin.clear(); // Clear any error flags
 
-            if (next_move > 9 || next_move < 1)
-                continue;
+            if (next_move > 9 || next_move < 1) // If move is out of range
+                continue; // Continue to prompt for input
 
-            position pos = get_position(next_move);
-            if (board[pos.i][pos.j] != ' ')
-                continue;
+            position pos = get_position(next_move); // Get the position on the board corresponding to the entered move
+            if (board[pos.i][pos.j] != ' ') // If the chosen position is already occupied
+                continue; // Continue to prompt for input
 
-            update_board(board, pos, 'X');
+            update_board(board, pos, 'X'); // Update the board with the player's move
             break;
         }
 
         display_board(board);
-        if (get_winner(board) != ' ') break;
+        if (get_winner(board) != ' ') break; // If there's a winner, exit the loop
 
-        comp.next_move(board);
+        comp.next_move(board); // Computer calculates and makes its move
         display_board(board);
     }
 
@@ -70,6 +71,19 @@ int main()
     if (winner == 'X') cout << "YOU WON!\n";
     else if (winner == 'O') cout << "COMPUTER WON!\n";
     else cout << "TIE!\n";
+
+    cout << endl;
+    char response;
+    cout << "Do you want to start a new game?(Y/N): ";
+    cin >> response;
+    cin.clear(); // Clear any error flags
+
+    if (response == 'y' || response == 'Ý') goto game_start; // If user wants to start a new game, jump to the game_start label
+
+    system("cls");
+    system("color 7"); // Set console text to white
+    cout << "GOOD BYE!... ";
+    cout << endl;
 
     return 0;
 }

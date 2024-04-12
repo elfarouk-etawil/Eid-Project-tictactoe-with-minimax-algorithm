3b16d810f4d66ec2edbf7aacc45a6c86269fc8d7
elfarouk-etawil <122612103+elfarouk-etawil@users.noreply.github.com>
2024-04-12 13:48:10 +0200

    Update board.h

--- board.h
+++ board.h
@@ -1,6 +1,7 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+// Function to reset the board to its initial state
 void reset_board(Board &b)
 {
     for (size_t i = 0; i < 3; i++)
@@ -8,15 +9,16 @@ void reset_board(Board &b)
             b[i][j] = ' ';
 }
 
+// Function to convert a move number to its corresponding position on the board
 inline position get_position(short n)
 {
     return {(n-1)/3, (n-1)%3};
 }
 
-
+// Function to display the current state of the game board
 void display_board(const Board b)
 {
-    system("cls");
+    system("cls"); // Clear the console screen
     cout << "+---+---+---+" << endl;
     cout << "| " << b[0][0] << " | " << b[0][1] << " | " << b[0][2] << " |" << endl;
     cout << "+---+---+---+" << endl;
@@ -26,11 +28,13 @@ void display_board(const Board b)
     cout << "+---+---+---+" << endl;
 }
 
+// Function to update the board with a player's move
 inline void update_board(Board& b, position p, char c)
 {
     b[p.i][p.j] = c;
 }
 
+// Function to count the number of empty spaces on the board
 uint16_t space_count(Board& b)
 {
     uint16_t cnt = 0;
@@ -40,6 +44,7 @@ uint16_t space_count(Board& b)
     return cnt;
 }
 
+// Function to determine the winner of the game
 char get_winner(Board& b)
 {
     char winner = ' ';
@@ -60,15 +65,15 @@ char get_winner(Board& b)
     return winner;
 }
 
+// Function to evaluate the current state of the board
 short evaluate(Board& board)
 {
     switch (get_winner(board))
     {
-    case ' ': return 0;
-    case 'X': return -1;
-    case 'O': return 1;
+    case ' ': return 0; // If the game is still ongoing
+    case 'X': return -1; // If the player wins
+    case 'O': return 1;  // If the computer wins
     }
 }
-
 
 #endif // BOARD_H
